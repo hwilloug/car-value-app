075219690236a06f965d3f8d9c8239e5d6216caa
Hannah Willoughby <hannah@tcah.com>
2022-09-15 15:55:28 -0400

    Add ability to get current user

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -8,7 +8,8 @@ import {
     Query, 
     Delete, 
     NotFoundException,
-    Session
+    Session,
+    UseInterceptors
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto.ts';
@@ -16,18 +17,27 @@ import { UsersService } from './users.service';
 import { UserDto } from './dtos/user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { User } from './user.entity'
 
 @Controller('auth')
 @Serialize(UserDto)
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
     constructor(
         private usersService: UsersService,
         private authService: AuthService
     ) {}
 
+    // @Get('/whoami')
+    // whoAmI(@Session() session: any) {
+    //     return this.usersService.findOne(session.userId)
+    // }
+
     @Get('/whoami')
-    whoAmI(@Session() session: any) {
-        return this.usersService.findOne(session.userId)
+    whoAmI(@CurrentUser() user: User) {
+        return user
     }
 
     @Post('/signup')
@@ -44,7 +54,7 @@ export class UsersController {
         return user
     }
 
-    @Post('signout')
+    @Post('/signout')
     signOut(@Session() session: any) {
         session.userId = null
     }
