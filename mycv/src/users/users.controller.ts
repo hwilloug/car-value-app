6616f020c09bd45501e4b05b1da440a15b48185f
Hannah Willoughby <hannah@tcah.com>
2022-09-15 16:07:49 -0400

    Add guards and refactor whoami interceptor

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -9,7 +9,7 @@ import {
     Delete, 
     NotFoundException,
     Session,
-    UseInterceptors
+    UseGuards
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto.ts';
@@ -18,24 +18,19 @@ import { UserDto } from './dtos/user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity'
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
     constructor(
         private usersService: UsersService,
         private authService: AuthService
     ) {}
 
-    // @Get('/whoami')
-    // whoAmI(@Session() session: any) {
-    //     return this.usersService.findOne(session.userId)
-    // }
-
     @Get('/whoami')
+    @UseGuards(AuthGuard)
     whoAmI(@CurrentUser() user: User) {
         return user
     }
